dc7407c91c55cdd6c6b180358c1adc1a16700869
Adolf <dgtronv@gmail.com>
2021-03-01 00:38:27 +0300

    feat: add reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    if (arguments.length === 0 || typeof n !== "number") {
+        return 0
+    } else {
+        let arr = `${Math.abs(n)}`.split('');
+        let result = [];
+
+        for (let i = 0; i < arr.length; i++) {
+            result.unshift(arr[i]);
+        }
+
+        return Number(result.join(''))
+    }
 }
